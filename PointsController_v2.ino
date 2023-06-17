4d6cdb16706da9410e61d1d4638948085fabf4ff
Matthew Dawkins <matthew.dawkins@gmail.com>
2023-06-17 15:42:17 +0100

    Add and set up servos, add comments, and fix all the bugs. srsly. dey all gone now

--- PointsController_v2.ino
+++ PointsController_v2.ino
@@ -2,79 +2,110 @@
 #include <ezButton.h>
 #include <Arduino.h>
 
+// Initialise the servo driver
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+/**
+* Servo class
+*/
 class Servo {
 public:
+  // Constructor
   Servo(
     String name,
     int servoPin,
     int buttonPin,
     int min,
     int max,
-    float start,
     float delta)
     : name(name),
       servoPin(servoPin),
       servoMin(min),
       servoMax(max),
       delta(delta),
-      current(start) {
-    pwm.setPWM(servoPin, 0, (int)current);
+      current(servoMin) {
+    pwm.setPWM(servoPin, 0, current);
     button = new ezButton(buttonPin);
-    button->setDebounceTime(200);
-    Serial.println(name + " ready");
+    button->setDebounceTime(50);
+    Serial.println(name + " ready: servo " + servoPin + ", button " + buttonPin);
   }
 
+  // Update the servo's positions
   void update() {
+    // Listen for a button press
     button->loop();
-    if (button->isPressed() && current < servoMax) {
-      current += delta;
-      pwm.setPWM(servoPin, 0, (int)current);
+
+    // If button is pressed and servo isn't at max position, move it forward a bit
+    if (!button->getState() && current < servoMax) {
+      current = current + delta;
+      pwm.setPWM(servoPin, 0, current);
       if (!moving) {
         moving = true;
         Serial.println(name + " activating");
       }
     }
-    if (button->isReleased() && current > servoMin) {
-      current -= delta;
-      pwm.setPWM(servoPin, 0, (int)current);
+
+    // If button is not pressed and servo isn't at min position, move it back a bit
+    if (button->getState() && current > servoMin) {
+      current = current - delta;
+      pwm.setPWM(servoPin, 0, current);
       if (!moving) {
         moving = true;
-        Serial.println(name + " deactivating");
+        Serial.println(name + " deactivating " + current);
       }
     }
+
+    // If we've reached min or max, stop
     if (moving && (current >= servoMax || current <= servoMin)) {
       moving = false;
-      Serial.println(name + " finished moving");
+      Serial.println(name + " finished moving, now at " + current);
     }
   }
 
 private:
-  String name;
-  int servoPin;      // Which output on the servo controller board
-  int servoMin;      // Minimum pulse length (150-600)
-  int servoMax;      // Maximum pulse length (150-600)
-  float delta;       // How much current changes on each iteration
-  ezButton *button;  // Button
-  float current;     // Current position
-  bool moving = false;
+  String name;          // Display name of what the servo operates
+  int servoPin;         // Which output on the servo controller board
+  int servoMin;         // Minimum pulse length (150-600)
+  int servoMax;         // Maximum pulse length (150-600)
+  float delta;          // How much current changes on each iteration
+  ezButton *button;     // Button
+  float current;        // Current position
+  bool moving = false;  // Whether the servo is currently moving
 };
 
-Servo *servos[4];
+// Set up an array of 9 uninstatiated Servo objects
+Servo *servos[9];
 
+// Do this once
 void setup() {
+  // Set up the servo driver board
   pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(50);
+  // Initialise the serial monitor
   Serial.begin(9600);
-  servos[0] = new Servo("Servo 1", 0, 2, 300, 400, 300, 1);
-  servos[1] = new Servo("Servo 2", 1, 3, 300, 400, 400, 2);
-  servos[2] = new Servo("Servo 3", 2, 4, 300, 400, 250, 0.5);
-  servos[3] = new Servo("Servo 4", 3, 5, 300, 400, 300, 0.1);
-  delay(1000);  // Deep breath before we begin...
+  // For my 9g servos the full 180 degree range is 100 to 500. YMMV.
+  servos[0] = new Servo("Point B: Yard Entrance", 0, 2, 300, 500, 1);
+  servos[1] = new Servo("Point C: Platform 2 / Loop", 1, 3, 315, 490, 1.5);
+  servos[2] = new Servo("Signal 1: Up Platform 1 Start", 2, 4, 230, 350, 1);
+  servos[3] = new Servo("Signal 2: Up Platform 2 Start", 3, 5, 300, 380, 1);
+  servos[4] = new Servo("Point 6: Platform 1 / Goods Siding", 4, 6, 300, 500, 1);
+  servos[5] = new Servo("Point 9: Platform 2 / Loop", 5, 7, 300, 500, 1);
+  servos[6] = new Servo("Point 14: Main / Sidings", 6, 8, 300, 500, 1);
+  servos[7] = new Servo("Signal 16: Down Platform 2 Inner Home", 7, 9, 300, 500, 1);
+  servos[8] = new Servo("Signal 17: Down Platform 1 Inner Home", 8, 10, 300, 500, 1);
+
+  // Deep breath before we begin...
+  delay(2000);
 }
 
+// Keep doing this
 void loop() {
-  for (int i = 0; i < 4; i++) {
-    servos[i]->update();
+  // Update each servo
+  for (auto servo : servos) {
+    servo->update();
   }
+
+  // Slow everything down a bit
+  delay(10);
 }
